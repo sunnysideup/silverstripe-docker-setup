8a3a0b9975caa9f5326c361efa9a0f1096dec2e6
Nicolaas <github@sunnysideup.co.nz>
2020-03-03 14:13:41 +1300

    Create startup.sh

---
+++ startup.sh
@@ -0,0 +1,28 @@
+# see https://www.digitalocean.com/community/tutorials/how-to-install-and-use-docker-on-ubuntu-18-04
+
+echo "1. install docker"
+sudo apt update
+sudo apt install apt-transport-https ca-certificates curl software-properties-common
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu bionic stable"
+sudo apt update
+apt-cache policy docker-ce
+sudo apt install docker-ce
+sudo systemctl status docker
+sudo usermod -aG docker ${USER}
+su - ${USER}
+id -nG
+
+echo "2. Install the latest version of docker-compose";
+```shell
+sudo curl -L https://github.com/docker/compose/releases/download/1.25.4/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
+docker-compose --version
+
+echo "3. stop services"
+
+sudo service apache2 stop
+sudo service mysql stop
+
+echo "4. start"
+docker-compose up -d
